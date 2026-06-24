df99507d8d89618a1cc760a375232388ee9a0ae0
Omar Abdiwali <omarabdiwali17@gmail.com>
2026-06-24 04:08:39 -0600

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
   return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
         <title>Pathfinding Visualizer</title>
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
